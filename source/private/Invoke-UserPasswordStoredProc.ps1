d5eb3ea5bd83befb4e718bbc88deccc0b1a1d0f4
James Ruskin <james@chocolatey.io>
2026-02-25 10:48:28 +0000

    Adds PostgreSQL Support to Invoke-UserPasswordStoredProc
    The new default database type as of 2025 versions of ProGet is PostgreSQL. Sadly, the existing stored procedure functions don't work with this.
    
    This commit adds a separate path to the function such that it can work with PostgreSQL successfully, as well as a mild refactor to reduce code duplication.

--- source/private/Invoke-UserPasswordStoredProc.ps1
+++ source/private/Invoke-UserPasswordStoredProc.ps1
@@ -6,79 +6,76 @@ function Invoke-UserPasswordStoredProc {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory)]
-        [Hashtable]        
-        $Params,
-        
-        [Parameter(DontShow)]
-        [String]
-        $ConnectionString = 'Server=Localhost\SQLEXPRESS;Database=ProGet;Trusted_Connection=true;'
+        [Hashtable]
+        $Params
     )
-
     end {
-        $StoredProcedure = 'dbo.Users_SetPassword'
-        if ($UseRemoting) {
-            # Execute on a remote machine using PowerShell remoting
-            $scriptBlock = {
-                param ($ConnectionString, $StoredProcedure, $Params)
-      
-                Add-Type -AssemblyName "System.Data"
-      
-                $connection = New-Object System.Data.SqlClient.SqlConnection
-                $connection.ConnectionString = $ConnectionString
-                $connection.Open()
-      
-                try {
+        $ScriptBlock = {
+            param ($ConnectionString, $Params, $GetDatabase)
+
+            $ConnectionType = & $GetDatabase | % Type
+
+            switch ($ConnectionType) {
+                "SQLServer" {
+                    Add-Type -AssemblyName "System.Data"
+
+                    $connection = New-Object System.Data.SqlClient.SqlConnection
+                    $connection.ConnectionString = $ConnectionString
+                    $connection.Open()
+
                     $command = $connection.CreateCommand()
                     $command.CommandType = [System.Data.CommandType]::StoredProcedure
-                    $command.CommandText = $StoredProcedure
+                    $command.CommandText = 'dbo.Users_SetPassword'
+
                     $command.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@User_Name", [Data.SqlDbType]::NVarChar, 50))).Value = $Params['User_Name']
                     $command.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@Password_Bytes", [Data.SqlDbType]::Binary, 20))).Value = $Params['Password_Bytes']
                     $command.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@Salt_Bytes", [Data.SqlDbType]::Binary, 10))).Value = $Params['Salt_Bytes']
-      
-                    $null = $command.ExecuteNonQuery()
+
+                    try {
+                        $null = $command.ExecuteNonQuery()
+                    } catch {
+                        Write-Error "An error occurred: $_"
+                    } finally {
+                        $connection.Close()
+                    }
                 }
-                catch {
-                    Write-Error "An error occurred: $_"
+                "PostgreSQL" {
+                    try {
+                        $TemporaryFile = New-TemporaryFile
+                        Set-Content -Path $TemporaryFile -Value @(
+                            'Call "Users_SetPassword"('
+                            "    '$($Params['User_Name'])'::varchar,"
+                            "    decode('$(-join($Params.Password_Bytes | ForEach-Object ToString X2))', 'hex'),"
+                            "    decode('$(-join($Params.Salt_Bytes | ForEach-Object ToString X2))', 'hex')"
+                            ');'
+                        )
+
+                        if (Resolve-Path $env:ProgramFiles\ProGet\Service\proget.exe) {
+                            $null = & (Join-Path $env:ProgramFiles "ProGet\Service\proget.exe") query --file="$($TemporaryFile.FullName)"
+                        } else {
+                            Write-Error "Could not find proget.exe"
+                        }
+                    } finally {
+                        Remove-Item $TemporaryFile.FullName
+                    }
                 }
-                finally {
-                    $connection.Close()
-                }
-            }
-      
-            if ($Credential) {
-                Invoke-Command -ComputerName $RemoteComputer -Credential $Credential -ScriptBlock $scriptBlock -ArgumentList $ConnectionString, $StoredProcedure, $Params
-            }
-            else {
-                Invoke-Command -ComputerName $RemoteComputer -ScriptBlock $scriptBlock -ArgumentList $ConnectionString, $StoredProcedure, $Params
             }
         }
-      
-        else {
+
+        if ($UseRemoting) {
+            # Execute on a remote machine using PowerShell remoting
+            $RemoteArgs = @{
+                ComputerName = $RemoteComputer
+                ScriptBlock  = $ScriptBlock
+                ArgumentList = $Connection.ConnectionString, $Params, (Get-Command Get-ProGetDatabase)
+            }
+            if ($Credential) {
+                $RemoteArgs.Credential = $Credential
+            }
+            Invoke-Command @RemoteArgs
+        } else {
             # Execute locally
-            Add-Type -AssemblyName "System.Data"
-      
-            $connection = New-Object System.Data.SqlClient.SqlConnection
-            $connection.ConnectionString = $ConnectionString
-            $connection.Open()
-      
-            try {
-                $command = $connection.CreateCommand()
-                $command.CommandType = [System.Data.CommandType]::StoredProcedure
-                $command.CommandText = $StoredProcedure
-      
-                $command.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@User_Name", [Data.SqlDbType]::NVarChar, 50))).Value =  $Params['User_Name']
-                $command.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@Password_Bytes", [Data.SqlDbType]::Binary, 20))).Value = $Params['Password_Bytes']
-                $command.Parameters.Add((New-Object Data.SqlClient.SqlParameter("@Salt_Bytes", [Data.SqlDbType]::Binary, 10))).Value = $Params['Salt_Bytes']
-  
-                $null = $command.ExecuteNonQuery()
-            }
-            catch {
-                Write-Error "An error occurred: $_"
-            }
-            finally {
-                $connection.Close()
-            }
+            & $ScriptBlock $Connection.ConnectionString $Params (Get-Command Get-ProGetDatabase)
         }
     }
-
 }
